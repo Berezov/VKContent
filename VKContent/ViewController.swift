1566387c9aff2c83fad6b2d0e4a8a2474ab92d0f
Berezov Alexey <lexx94.nsk@gmail.com>
2019-03-17 22:04:29 +0700

    Add xcode progect

---
+++ VKContent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKContent
+//
+//  Created by Apple on 17/03/2019.
+//  Copyright © 2019 Alexey Berezov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
